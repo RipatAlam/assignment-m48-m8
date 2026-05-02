fe1fb52991963bbeec98bacbb9e1c1f03406c809
RipatAlam <mdalamm5090@gmail.com>
2026-05-02 11:44:16 +0300

    assignment-m48-m8

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import HeroSectionPage from "@/components/HeroSection";
-import FeaturedBooks from "./featuredbooks/page";
 
 export default function Home() {
   return (
     <div>
       <HeroSectionPage />
-      <FeaturedBooks />
     </div>
   );
 }
